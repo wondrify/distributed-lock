896cba6a411e0d417aa7021dc8ed282a4fa14f7d
David Estes <davydotcom@gmail.com>
2024-12-13 08:08:00 -0500

    preparing for grails7

--- distributed-lock-gorm/grails-app/domain/com/bertram/lock/DistributedLock.groovy
+++ distributed-lock-gorm/grails-app/domain/com/bertram/lock/DistributedLock.groovy
@@ -3,6 +3,8 @@ package com.bertram.lock
 class DistributedLock {
 	String name
 	String value
+	Integer count=0
+	Integer maxCount=0
 	Long timeout //epoch timeout
 
 	static mapping = {
@@ -13,5 +15,7 @@ class DistributedLock {
     	name unique:true, nullable:false
     	value nullable:true
     	timeout nullable:true
+			maxCount nullable: true
+			count nullable: true
     }
 }
